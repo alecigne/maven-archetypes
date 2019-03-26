29b4488f309df8431869f4b763e19a305be5c35b
Anthony Le Cigne <dev@lecigne.net>
2019-03-26 17:26:46 +0100

    Fix import in test class

--- java8-junit5-archetype/src/main/resources/archetype-resources/src/test/java/AppTest.java
+++ java8-junit5-archetype/src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -1,6 +1,6 @@
 package ${package};
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
