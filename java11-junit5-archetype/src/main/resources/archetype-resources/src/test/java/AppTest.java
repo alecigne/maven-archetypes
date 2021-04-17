d5d467b4f7031a1f5e6f8d34a1bcb75ca68956df
Anthony Le Cigne <dev@lecigne.net>
2021-04-17 11:18:52 +0200

    Add java11-junit5 archetype

---
+++ java11-junit5-archetype/src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -0,0 +1,14 @@
+package ${package};
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+    @Test
+    void dummyTest() {
+        assertTrue(true);
+    }
+
+}
